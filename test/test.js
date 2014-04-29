3288eca16e31633b204e4b709da39133ffcfcd65
dchack <hdbysj@163.com>
2014-04-29 20:04:58 +0800

    线上可测服务器

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@
  */
 var open = require('../lib/wopen');
 
-var secret = '4b6f5598be6545b3b2756f923d6d8a49';
-var app_key = '10058';
+var secret = '3431355427c1451787b3398836349226';
+var app_key = '21103';
 var data = {
     merchantId : '003214a2bc7111e2801200163e122bbb',
     appKey : app_key
