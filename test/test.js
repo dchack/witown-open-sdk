18925e787ed6875ba805472db789d0918e7b8bba
dongchao <hdbysj@163.com>
2014-05-04 22:51:21 +0800

    modified:   lib/http_client.js 	modified:   lib/wopen.js 	modified:   test/test.js

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
  * just for test
  */
 var open = require('../lib/wopen');
-
+var client = require('../lib/http_client');
 var secret = '3431355427c1451787b3398836349226';
 var app_key = '21103';
 var data = {
@@ -10,8 +10,10 @@ var data = {
     appKey : app_key
 };
 // merchant.get接口例子
+//open.open_api(data, secret, function(err, response){
+//    console.log(response);
+//}).merchant.get();
+
 open.open_api(data, secret, function(err, response){
     console.log(response);
-}).merchant.get();
-
-
+}).user.get();
